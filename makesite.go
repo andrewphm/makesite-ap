a330b6ad7e385e1ebabaf34b55ea69a0d3869c6f
Andrew Pham <andrew.pham@shopify.com>
2024-04-03 21:56:32 -0400

    Read from text file, and write out a html file from a template

--- makesite.go
+++ makesite.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"html/template"
+)
+
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content	     string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	textContent, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		fmt.Println("Error reading file.")
+		panic(err)
+	}
+
+	page := Page{
+		TextFilePath: "first-post.txt",
+		TextFileName: "first-post",
+		HTMLPagePath: "first-post.html",
+		Content: string(textContent),
+	}
+
+	t, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		fmt.Println("Error parsing template.")
+		panic(err)
+	}
+
+	file, err := os.Create(page.HTMLPagePath)
+	if err != nil {
+		fmt.Println("Error creating file.")
+		panic(err)
+	}
+	defer file.Close()
+
+	err = t.Execute(file, page)
+	if err != nil {
+		fmt.Println("Error executing template.")
+		panic(err)
+	}
+
+	// print to Stdout
+	err = t.Execute(os.Stdout, page)
+	if err != nil {
+		fmt.Println("Error executing template.")
+		panic(err)
+	}
+
 }
