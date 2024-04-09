e490297535a9a74055a1312eef1e1c7e1651b320
Andrew Pham <andrew.pham@shopify.com>
2024-04-09 08:37:25 -0400

    Create new 3 new test text files. And new dir flag. Add functionality to generate HTML files from a list of text files in any given directory

--- makesite.go
+++ makesite.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"os"
-	"html/template"
-	"flag"
+	"path/filepath"
 	"strings"
 )
 
@@ -13,52 +14,80 @@ type Page struct {
 	TextFilePath string
 	TextFileName string
 	HTMLPagePath string
-	Content	     string
+	Content      string
 }
 
 func main() {
-	fileNameFlag := flag.String("file", "first-post.txt", "The name of the file to convert to HTML.")
-
+	dirFlag := flag.String("dir", "", "The directory to search for text files")
+	fileFlag := flag.String("file", "", "The single text file to process")
 	flag.Parse()
-	fileNameWithoutExtension := strings.TrimSuffix(*fileNameFlag, ".txt")
 
-	textContent, err := ioutil.ReadFile(*fileNameFlag)
-	if err != nil {
-		fmt.Println("Error reading file.")
+
+	htmlDir := "html_pages"
+	if err := os.MkdirAll(htmlDir, os.ModePerm); err != nil {
+		fmt.Printf("Error creating directory %s: %v\n", htmlDir, err)
 		panic(err)
 	}
+
+	if *fileFlag != "" {
+		processFile(*fileFlag, htmlDir)
+	} else if *dirFlag != "" {
+		files, err := ioutil.ReadDir(*dirFlag)
+		if err != nil {
+			fmt.Printf("Error reading directory %s: %v\n", *dirFlag, err)
+			panic(err)
+		}
+		for _, file := range files {
+			if !file.IsDir() && strings.HasSuffix(file.Name(), ".txt") {
+				fmt.Println(file.Name())
+				filePath := filepath.Join(*dirFlag, file.Name())
+				processFile(filePath, htmlDir)
+			}
+		}
+	} else {
+		fmt.Println("No file or directory specified. Use --file or --dir flag.")
+	}
+}
+
+func processFile(filePath, htmlDir string) {
+	fileName := filepath.Base(filePath)
+	fileNameWithoutExt := strings.TrimSuffix(fileName, ".txt")
+	htmlFilePath := filepath.Join(htmlDir, fileNameWithoutExt+".html")
 
 	page := Page{
-		TextFilePath: *fileNameFlag,
-		TextFileName: fileNameWithoutExtension,
-		HTMLPagePath: fileNameWithoutExtension + ".html",
-		Content: string(textContent),
+		TextFilePath: filePath,
+		TextFileName: fileNameWithoutExt,
+		HTMLPagePath: htmlFilePath,
+		Content:      readFileContent(filePath),
 	}
 
+	generateHTMLPage(page)
+}
+
+func readFileContent(filePath string) string {
+	contentBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("Error reading file %s: %v\n", filePath, err)
+		return ""
+	}
+	return string(contentBytes)
+}
+
+func generateHTMLPage(page Page) {
 	t, err := template.ParseFiles("template.tmpl")
 	if err != nil {
-		fmt.Println("Error parsing template.")
-		panic(err)
+		fmt.Printf("Error parsing template for file %s: %v\n", page.TextFilePath, err)
+		return
 	}
 
 	file, err := os.Create(page.HTMLPagePath)
 	if err != nil {
-		fmt.Println("Error creating file.")
-		panic(err)
+		fmt.Printf("Error creating HTML file %s: %v\n", page.HTMLPagePath, err)
+		return
 	}
 	defer file.Close()
 
-	err = t.Execute(file, page)
-	if err != nil {
-		fmt.Println("Error executing template.")
-		panic(err)
+	if err := t.Execute(file, page); err != nil {
+		fmt.Printf("Error executing template for file %s: %v\n", page.TextFilePath, err)
 	}
-
-	// print to Stdout
-	err = t.Execute(os.Stdout, page)
-	if err != nil {
-		fmt.Println("Error executing template.")
-		panic(err)
-	}
-
 }
